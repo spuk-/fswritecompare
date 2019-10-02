2d9338410c03b14b8f2cb5558da4a4f8574cfa4e
spuk <spuk@localhost>
2019-10-02 03:49:00 -0300

    Including ext3, and fixing the sync.

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+umount /tmp/btrfs /tmp/ext4 /tmp/ext3 /tmp/xfs
 dd if=/dev/zero of=/tmp/btrfs.img bs=1M count=200
 dd if=/dev/zero of=/tmp/ext4.img bs=1M count=200
+dd if=/dev/zero of=/tmp/ext3.img bs=1M count=200
 dd if=/dev/zero of=/tmp/xfs.img bs=1M count=200
 mkfs.btrfs /tmp/btrfs.img
 mkfs.ext4 /tmp/ext4.img
+mkfs.ext3 /tmp/ext3.img
 mkfs.xfs /tmp/xfs.img
-mkdir /tmp/btrfs /tmp/ext4 /tmp/xfs
-mount -o loop /tmp/btrfs.img /tmp/btrfs
+mkdir /tmp/btrfs /tmp/ext4 /tmp/ext3 /tmp/xfs
+mount -o loop,compress /tmp/btrfs.img /tmp/btrfs
 mount -o loop /tmp/ext4.img /tmp/ext4
+mount -o loop /tmp/ext3.img /tmp/ext3
 mount -o loop /tmp/xfs.img /tmp/xfs
--- test.sh
+++ test.sh
@@ -4,10 +4,11 @@ echo "== PRE COPY =="
 ./diskstats.sh
 dd if=/dev/urandom of=/tmp/urnd bs=3000000 count=1 2>/dev/null
 cp -f /tmp/urnd /tmp/ext4/
+cp -f /tmp/urnd /tmp/ext3/
 cp -f /tmp/urnd /tmp/xfs/
 cp -f /tmp/urnd /tmp/btrfs/
 echo "== POST COPY / PRE SED =="
 ./diskstats.sh
-sed -r -i -e 's/5/9/g' /tmp/ext4/urnd /tmp/xfs/urnd /tmp/btrfs/urnd
+sed -r -i -e 's/5/9/g' /tmp/ext4/urnd /tmp/ext3/urnd /tmp/xfs/urnd /tmp/btrfs/urnd
 echo "== POST SED =="
 ./diskstats.sh
