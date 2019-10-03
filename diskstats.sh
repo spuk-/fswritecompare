4523473bc6673f3c1299e4f33005dd8e395e9ebb
spuk <spuk@localhost>
2019-10-03 01:15:08 -0300

    Easier to deal with shellscript only, rather than mixing with awk.

--- diskstats.sh
+++ diskstats.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 sync -f /tmp/ext4/ /tmp/ext3/ /tmp/xfs/ /tmp/btrfs/
-losetup -nal | while read dev q q q q fs resto; do awk "{ printf \"%20.20s %s\\n\",\"$fs\",\$7 }" /sys/class/block/${dev##*/}/stat; done
+losetup -nal | while read dev q q q q fs q; do
+  read q q q q q q wrsects q </sys/class/block/${dev##*/}/stat
+  printf "%20.20s %s\n" $fs $wrsects
+done
 
