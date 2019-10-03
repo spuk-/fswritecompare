41fc18724f450c1a5a7780d82239fd803b9fc17f
spuk <spuk@localhost>
2019-10-03 01:05:37 -0300

    Lets prefer /sys over /proc.

--- diskstats.sh
+++ diskstats.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sync -f /tmp/ext4/ /tmp/ext3/ /tmp/xfs/ /tmp/btrfs/
-losetup | while read dev q q q q fs resto; do awk "/${dev##/dev/}/ { printf \"%20.20s %s\\n\",\"$fs\",\$10 }" /proc/diskstats; done
+losetup -nal | while read dev q q q q fs resto; do awk "{ printf \"%20.20s %s\\n\",\"$fs\",\$7 }" /sys/class/block/${dev##*/}/stat; done
 
