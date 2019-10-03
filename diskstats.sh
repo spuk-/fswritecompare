2fc3973fd473d6b47b328431811bb632b80706c6
spuk <spuk@localhost>
2019-10-03 01:35:34 -0300

    Format in columns for easier viewing/comparison.

--- diskstats.sh
+++ diskstats.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 sync -f /tmp/ext4/ /tmp/ext3/ /tmp/xfs/ /tmp/btrfs/
+# Show header
+losetup -nal | while read dev q q q q fs q; do
+  printf "%20.20s   " $fs
+done
+echo
+# Show numbers
 losetup -nal | while read dev q q q q fs q; do
   read q q q q q q wrsects q </sys/class/block/${dev##*/}/stat
-  printf "%20.20s %s\n" $fs $wrsects
+  printf "%20.20s   " $wrsects
 done
-
+echo
