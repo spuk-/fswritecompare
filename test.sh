1eb515bc559448fd7509429ea7f3e3cba0451468
spuk <spuk@localhost>
2019-10-04 02:52:55 -0300

    More tests.

--- test.sh
+++ test.sh
@@ -23,5 +23,26 @@ echo "== POST COPY / PRE WRITESIZE MUTATION =="
 for fs in "${FS[@]}"; do
   dd if=/tmp/urnd bs=$WRITESIZE count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=$WRITESIZE count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
 done
+echo "== POST WRITESIZE MUTATION / PRE 1 SECTOR MUTATION =="
+./diskstats.sh
+for fs in "${FS[@]}"; do
+  dd if=/tmp/urnd bs=512 count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=512 count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
+done
+echo "== POST 1 SECTOR MUTATION / PRE 4kb MUTATION =="
+./diskstats.sh
+for fs in "${FS[@]}"; do
+  dd if=/tmp/urnd bs=4k count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=4k count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
+done
+echo "== POST 4kb MUTATION / PRE OVERLAPPING 1 SECTOR + 4kb MUTATION =="
+./diskstats.sh
+for fs in "${FS[@]}"; do
+  dd if=/tmp/urnd bs=512 count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=512 count=1 seek=$SEEK conv=notrunc 2>/dev/null
+  dd if=/tmp/urnd bs=4k count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=512 count=4 seek=$SEEK conv=notrunc 2>/dev/null
+done
+echo "== POST 4kb MUTATION / PRE APPEND =="
+./diskstats.sh
+for fs in "${FS[@]}"; do
+  dd if=/tmp/urnd bs=$WRITESIZE count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=$WRITESIZE count=1 conv=notrunc oflag=sync,append 2>/dev/null
+done
 echo "== POST MUTATION =="
 ./diskstats.sh
