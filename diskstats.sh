1163514888b6d0fc91ae89ab408ef895500494a5
spuk <spuk@localhost>
2019-10-09 04:18:10 -0300

    Better formatting. Possibility of not showing headers.

--- diskstats.sh
+++ diskstats.sh
@@ -9,14 +9,16 @@ fi
 for fs in "${FS[@]}"; do
     sync -f "/tmp/$fs"
 done
-# Show header
-losetup -nal | sort | while read dev q q q q fs q; do
-  printf "%20.20s   " $fs
-done
-echo
+if [ -z "$NOHEADERS" ]; then
+    # Show header
+    losetup -nal | sort | while read dev q q q q fs q; do
+      printf "%10.10s   " ${fs##*/}
+    done
+    echo
+fi
 # Show numbers
 losetup -nal | sort | while read dev q q q q fs q; do
   read q q q q q q wrsects q </sys/class/block/${dev##*/}/stat
-  printf "%20.20s   " $wrsects
+  printf "%10.10s   " $wrsects
 done
 echo
