ed105182d673781d51d42fe2158d16fb209cb89c
spuk <spuk@localhost>
2019-10-09 04:18:43 -0300

    Tests for uncompressible and highly compressible data.

--- test.sh
+++ test.sh
@@ -12,37 +12,46 @@ fi
 
 echo "FSIZE=$FSIZE SEEK=$SEEK WRITESIZE=$WRITESIZE"
 
-echo "== PRE COPY =="
-./diskstats.sh
-dd if=/dev/urandom of=/tmp/urnd bs=$FSIZE count=1 2>/dev/null
-for fs in "${FS[@]}"; do
-    cp -f /tmp/urnd "/tmp/$fs/"
+# Make random (uncompressible) file
+dd if=/dev/urandom of="/tmp/urnd" bs=$FSIZE count=1 2>/dev/null
+# Make compressible file
+dd if=/dev/zero bs=$FSIZE count=1 2>/dev/null | tr '\0' '1' >"/tmp/uns"
+
+
+for srcf in uns urnd; do
+    SRCF="$srcf"
+
+    echo "== PRE COPY ($SRCF) =="
+    ./diskstats.sh
+    for fs in "${FS[@]}"; do
+        cp -f "/tmp/$SRCF" "/tmp/$fs/"
+    done
+    echo "== POST COPY / PRE WRITESIZE MUTATION ($SRCF) =="
+    NOHEADERS=1 ./diskstats.sh
+    for fs in "${FS[@]}"; do
+      dd if="/tmp/$SRCF" bs=$WRITESIZE count=1 2>/dev/null | dd of="/tmp/$fs/$SRCF" bs=$WRITESIZE count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
+    done
+    echo "== POST WRITESIZE MUTATION / PRE 1 SECTOR MUTATION ($SRCF) =="
+    NOHEADERS=1 ./diskstats.sh
+    for fs in "${FS[@]}"; do
+      dd if="/tmp/$SRCF" bs=512 count=1 2>/dev/null | dd of="/tmp/$fs/$SRCF" bs=512 count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
+    done
+    echo "== POST 1 SECTOR MUTATION / PRE 4kb MUTATION ($SRCF) =="
+    NOHEADERS=1 ./diskstats.sh
+    for fs in "${FS[@]}"; do
+      dd if="/tmp/$SRCF" bs=4k count=1 2>/dev/null | dd of="/tmp/$fs/$SRCF" bs=4k count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
+    done
+    echo "== POST 4kb MUTATION / PRE OVERLAPPING 1 SECTOR + 4kb MUTATION ($SRCF) =="
+    NOHEADERS=1 ./diskstats.sh
+    for fs in "${FS[@]}"; do
+      dd if="/tmp/$SRCF" bs=512 count=1 2>/dev/null | dd of="/tmp/$fs/$SRCF" bs=512 count=1 seek=$SEEK conv=notrunc 2>/dev/null
+      dd if="/tmp/$SRCF" bs=4k count=1 2>/dev/null | dd of="/tmp/$fs/$SRCF" bs=512 count=4 seek=$SEEK conv=notrunc 2>/dev/null
+    done
+    echo "== POST 4kb MUTATION / PRE APPEND ($SRCF) =="
+    NOHEADERS=1 ./diskstats.sh
+    for fs in "${FS[@]}"; do
+      dd if="/tmp/$SRCF" bs=$WRITESIZE count=1 2>/dev/null | dd of="/tmp/$fs/$SRCF" bs=$WRITESIZE count=1 conv=notrunc oflag=sync,append 2>/dev/null
+    done
+    echo "== POST APPEND ($SRCF) =="
+    NOHEADERS=1 ./diskstats.sh
 done
-echo "== POST COPY / PRE WRITESIZE MUTATION =="
-./diskstats.sh
-for fs in "${FS[@]}"; do
-  dd if=/tmp/urnd bs=$WRITESIZE count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=$WRITESIZE count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
-done
-echo "== POST WRITESIZE MUTATION / PRE 1 SECTOR MUTATION =="
-./diskstats.sh
-for fs in "${FS[@]}"; do
-  dd if=/tmp/urnd bs=512 count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=512 count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
-done
-echo "== POST 1 SECTOR MUTATION / PRE 4kb MUTATION =="
-./diskstats.sh
-for fs in "${FS[@]}"; do
-  dd if=/tmp/urnd bs=4k count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=4k count=1 seek=$SEEK conv=notrunc oflag=sync 2>/dev/null
-done
-echo "== POST 4kb MUTATION / PRE OVERLAPPING 1 SECTOR + 4kb MUTATION =="
-./diskstats.sh
-for fs in "${FS[@]}"; do
-  dd if=/tmp/urnd bs=512 count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=512 count=1 seek=$SEEK conv=notrunc 2>/dev/null
-  dd if=/tmp/urnd bs=4k count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=512 count=4 seek=$SEEK conv=notrunc 2>/dev/null
-done
-echo "== POST 4kb MUTATION / PRE APPEND =="
-./diskstats.sh
-for fs in "${FS[@]}"; do
-  dd if=/tmp/urnd bs=$WRITESIZE count=1 2>/dev/null | dd of="/tmp/$fs/urnd" bs=$WRITESIZE count=1 conv=notrunc oflag=sync,append 2>/dev/null
-done
-echo "== POST APPEND =="
-./diskstats.sh
